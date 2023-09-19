5cda03897f0c0c7c8e7a428d5c3fa051c4854e3f
Caro Viggiano <caroviggiano05@gmail.com>
2023-09-18 23:15:32 -0300

    b

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css"
+import img from "./logo-ico.ico"
+
+
+const Header = (props) =>  {
+    return (
+    <div className= "Header"> 
+    <img src= {img} alt="logo-alba" />
+    <h1>{props.title}</h1>
+    <h2>{props.subtitle}</h2>
+    </div>
+    );
+};
+
+export default Header
